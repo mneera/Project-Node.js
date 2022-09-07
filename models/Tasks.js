8fc1499b89b81466fcdde8b1e2074eebc9beceef
Muneera <52115195+mneera@users.noreply.github.com>
2022-09-08 02:04:22 +0300

    Add files via upload

--- models/Tasks.js
+++ models/Tasks.js
@@ -11,15 +11,10 @@ const TasksSchema = new Schema(
     AssignedDate: Date,
     CompletedDate: Date,
     Point: Number,
-    Status: { type: Schema.Types.ObjectId, ref: "Status" },
+    Status: { type: Schema.Types.ObjectId, ref: "Status",
+    "default": "6317c8badd1f80d5e34b09de" },
+
     assigned: [{ type: Schema.Types.ObjectId, ref: "Users" }],
-    
-    comments: [
-      {
-        author: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
-        content: String,
-      },
-    ],
   },
   {
     timestamp: true,
--- models/Users.js
+++ models/Users.js
@@ -1,35 +1,35 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Tasks = require("./Tasks");
-const Role = require("./Role");
 
 const UsersSchema = new Schema(
   {
-    UserName: String,
+    email: String,
     password: String,
     Name: String,
     role: { type: Schema.Types.ObjectId, ref: "Role" },
-    email:  String,
-    
+
     Tasks: [
       {
         Task: { type: mongoose.Schema.Types.ObjectId, ref: "Tasks" },
-        marks: Number,
+        Disabled: {
+          type: Boolean,
+          default: "No",
+        },
       },
     ],
   },
   {
-    timestamp: true, 
+    timestamp: true,
     statics: {
       findByName(name) {
-        return this.find({ name: new RegExp(name, 'i') });
-      }
-  } 
-}
+        return this.find({ name: new RegExp(name, "i") });
+      },
+    },
+  }
 );
 
 // UsersSchema.static("findByName", function (Name) {
-//   return this.find({ Name: Name });
+// return this.find({ Name: Name });
 // });
 
 const Users = mongoose.model("Users", UsersSchema);
