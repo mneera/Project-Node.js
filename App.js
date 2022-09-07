6da1d7ea9db5baeb863e6d6d6469276611dc48a0
Muneera <52115195+mneera@users.noreply.github.com>
2022-09-08 02:05:35 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -3,27 +3,18 @@ const mongoose = require("mongoose");
 const TaksRouter = require("./routes/Tasks");
 const UsersRouter = require("./routes/Users");
 const RoleTouter = require("./routes/Role");
-const StatusTouter = require("./routes/Status")
+const StatusTouter = require("./routes/Status");
 
 require("dotenv").config();
 const app = express();
 app.use(express.json());
 
 mongoose.connect(process.env.CNSTRING, { useNewUrlParser: true });
-// mongoose
-//   .connect(process.env.CNSTRING, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then((result) => app.listen(3000))
-//   .catch((err) => console.log(err));
 
 app.use("/Tasks", TaksRouter);
 app.use("/Users", UsersRouter);
 app.use("/Role", RoleTouter);
 app.use("/Status", StatusTouter);
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
